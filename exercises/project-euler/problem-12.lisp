fdbfda46f38328358132ac02f995c74d878b7202
Lucas Vieira <lucasvieira@protonmail.com>
2019-07-23 11:43:08 -0300

    ProjectEuler: Improved Problem 12.

--- exercises/project-euler/problem-12.lisp
+++ exercises/project-euler/problem-12.lisp
@@ -1,8 +1,20 @@
-(defun count-factors (number)
+(defun count-factors-old (number)
   "Counts the number of factors of a number."
   (if (> number 1)
       (1+ (loop for i from 1 to (floor number 2)
 	     count (zerop (mod number i))))
+      1))
+
+(defun count-factors (number)
+  "Optimized version of count-factors-old. Takes into
+consideration that you only need to go as far as the
+square root of the number. Much faster as well!"
+  (if (> number 1)
+      (let ((num-factors 0))
+	(loop for i from 1 to (floor (sqrt number))
+	   when (zerop (mod number i))
+	   do (incf num-factors 2))
+	num-factors)
       1))
 
 (defun find-triangle-number-prob12 (num-divisors)
@@ -15,6 +27,7 @@ NUM-DIVISORS divisors."
        return triangle-num)))
 	 
 
-;; Answer  ... ahem, it might take some time to run.
-;; Took about an hour on my intel i7 2nd gen.
+;; Answer.
+;; count-factors-old: Took about an hour on my intel i7 2nd gen.
+;; count-factors:     Took about one second. YIKES!
 (find-triangle-number-prob12 500)
