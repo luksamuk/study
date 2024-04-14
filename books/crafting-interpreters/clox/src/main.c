9c208d5ebbe0662e68ccfc7277b20f7ac5064724
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-04-14 19:37:19 -0300

    Add some more stuff

--- books/crafting-interpreters/clox/src/main.c
+++ books/crafting-interpreters/clox/src/main.c
@@ -1,4 +1,5 @@
 #include <clox/foo.h>
+#include <clox/linked_list.h>
 #include <stdio.h>
 
 int
@@ -6,6 +7,34 @@ main(void)
 {
     int result = sum(2, 3);
     printf("Result = %d\n", result);
+
+    linkedlist list = make_linkedlist();
+    linkedlist_insert(&list, "foo");
+    linkedlist_insert(&list, "bar");
+    linkedlist_insert(&list, "test");
+
+    struct LINKEDLIST_NODE *aux;
+
+    aux = list.root;
+    while(aux) {
+        printf("%s\n", aux->info);
+        aux = aux->next;
+    }
+
+    linkedlist_delete(&list, "foo");
+
+    printf("Number of elements: %u\n", linkedlist_count(&list));
+    
+    aux = list.root;
+    while(aux) {
+        printf("%s\n", aux->info);
+        aux = aux->next;
+    }
+    
+    printf("Number of elements: %u\n", linkedlist_count(&list));
+
+    destroy_linkedlist(&list);
+
     return 0;
 }
 
