20e41429b54de0974ff40929a06e126804be3381
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-04-28 02:48:38 -0300

    PSX: Emulation & Debugger

---
+++ courses/psx-programming/src/fillmem.s
@@ -0,0 +1,17 @@
+	.psx
+	.create "fillmem.bin", 0x80010000
+	.org 0x80010000
+
+	BASE_ADDR equ 0x0000
+
+Main:
+	li	$t0, 0xa000
+	li	$t1, 0xa0ff
+	li	$t2, 0x11
+
+Loop:
+	sb	$t2, BASE_ADDR($t0)
+	addi	$t0, $t0, 1
+	ble	$t0, $t1, Loop
+End:
+	.close
