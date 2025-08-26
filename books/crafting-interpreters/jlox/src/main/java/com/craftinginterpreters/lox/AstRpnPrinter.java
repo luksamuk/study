5b0936b1d5cfb1997e4b1f783b73fe9e581a3bef
Lucas Vieira <lucasvieira@protonmail.com>
2025-08-25 23:48:42 -0300

    [CraftingInterpreters] Add notes and exercises from chapter 5

---
+++ books/crafting-interpreters/jlox/src/main/java/com/craftinginterpreters/lox/AstRpnPrinter.java
@@ -0,0 +1,56 @@
+package com.craftinginterpreters.lox;
+
+/// This printer is temporary.
+/// WARNING: This file is removed from IntelliJ IDEA's compilation configuration by default
+
+public class AstRpnPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) +
+                " " +
+                expr.right.accept(this) +
+                " " +
+                expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if(expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Grouping(
+                        new Expr.Binary(
+                                new Expr.Literal(1),
+                                new Token(TokenType.PLUS, "+", null, 1),
+                                new Expr.Literal(2)
+                        )
+                ),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Binary(
+                                new Expr.Literal(4),
+                                new Token(TokenType.MINUS, "-", null, 1),
+                                new Expr.Literal(3)
+                        )
+                )
+        );
+        System.out.println(new AstRpnPrinter().print(expression));
+    }
+}
