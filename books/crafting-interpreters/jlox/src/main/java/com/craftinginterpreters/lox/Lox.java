a460805a4705a3e088d5872fbcb5570b289dab80
Lucas Vieira <lucasvieira@protonmail.com>
2025-09-13 18:23:05 -0300

    [CraftingInterpreters] Add Chapter 7 notes

--- books/crafting-interpreters/jlox/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ books/crafting-interpreters/jlox/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if(args.length > 1) {
@@ -26,6 +28,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if(hadError) System.exit(65);
+        if(hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -48,8 +51,9 @@ public class Lox {
 
         if(hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
-        System.out.println(new AstRpnPrinter().print(expression));
+        //System.out.println(new AstPrinter().print(expression));
+        //System.out.println(new AstRpnPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -62,6 +66,12 @@ public class Lox {
             return;
         }
         report(token.line, " at '" + token.lexeme + "'", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
